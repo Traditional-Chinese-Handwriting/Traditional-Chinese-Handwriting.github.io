d2fc0fa01e3c7a38f0ff85eb4a4caa30d26195ed
chenkenanalytic <k826292000@hotmail.com>
2021-03-18 13:51:54 +0800

    Update TCHC-data.js

--- TCHC-data.js
+++ TCHC-data.js
@@ -55,7 +55,7 @@ export class FMnistData {
         const datasetBytesBuffer =
             new ArrayBuffer(NUM_DATASET_ELEMENTS * IMAGE_SIZE * 4);
 
-        const chunkSize = 5000;
+        const chunkSize = 12;
         canvas.width = img.width;
         canvas.height = chunkSize;
 
